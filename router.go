095cc8dd5841e5a6754ada363d63acde008cbf06
Kay-Zee <kan@axiomzen.co>
2017-01-19 18:10:50 -0800

    Write out routes

--- router.go
+++ router.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"strconv"
+
 	"github.com/gocraft/web"
 )
 
@@ -13,4 +15,44 @@ func (c *MyContext) Ping(rw web.ResponseWriter, req *web.Request) {
 		return
 	}
 	c.render(http.StatusOK, Message{Message: "pong"}, rw)
+}
+
+// GetQuestions gets a list of questions
+func (c *MyContext) GetQuestions(rw web.ResponseWriter, req *web.Request) {
+	var questions Questions
+	if err := db.Model(&questions).Select(); err != nil {
+		c.render(http.StatusBadRequest, nil, rw)
+		return
+	}
+	c.render(http.StatusOK, questions, rw)
+}
+
+// AskQuestions saves a question to the db
+func (c *MyContext) AskQuestions(rw web.ResponseWriter, req *web.Request) {
+	var question Question
+	if err := c.decode(&question, req.Body); err != nil {
+		c.render(http.StatusBadRequest, nil, rw)
+		return
+	}
+	if err := db.Insert(&question); err != nil {
+		c.render(http.StatusBadRequest, nil, rw)
+		return
+	}
+	c.render(http.StatusOK, question, rw)
+}
+
+// Upvote allows adding an upvote to the question
+func (c *MyContext) Upvote(rw web.ResponseWriter, req *web.Request) {
+	questionID, _ := strconv.ParseInt(req.PathParams["question_id"], 10, 64)
+	question := Question{
+		ID: questionID,
+	}
+	if res, err := db.Model(&question).Set("votes = votes + 1").Where("id = ?id").Update(); err != nil {
+		c.render(http.StatusBadRequest, Message{Message: "Malformed body"}, rw)
+		return
+	} else if res.RowsAffected() != 1 {
+		c.render(http.StatusNotFound, Message{Message: "Question not found"}, rw)
+		return
+	}
+	c.render(http.StatusNoContent, nil, rw)
 }

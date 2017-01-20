223bc82d37a0a731d1a4eea3d058ceda47197c6d
Kay-Zee <kan@axiomzen.co>
2017-01-19 17:58:46 -0800

    Basic db connection

--- main.go
+++ main.go
@@ -4,10 +4,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"net/url"
+
+	pg "gopkg.in/pg.v5"
 )
 
+var db *pg.DB
+
 func main() {
+
+	dbURL, _ := url.Parse(os.Getenv("DATABASE_URL"))
+
+	dbOpts := &pg.Options{
+		User:     dbURL.User.Username(),
+		Database: dbURL.Path[1:],
+		Addr:     dbURL.Host,
+	}
+
+	if dbPass, ok := dbURL.User.Password(); ok {
+		dbOpts.Password = dbPass
+	}
+
+	db = pg.Connect(dbOpts)
+
 	port := os.Getenv("PORT")
+
 	server := http.Server{
 		Addr: ":" + port,
 	}

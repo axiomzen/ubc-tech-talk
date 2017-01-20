bfc7c07d46cfe4d143be1cc1aa27b26d73378d0a
Christine Legge <christine@axiomzen.co>
2017-01-19 18:24:59 -0800

    create index.html

---
+++ src/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends React.Component {
+
+  render() {
+    return (
+      <div>hi</div>
+    );
+  }
+
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
